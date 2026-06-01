9e71673a35d9f83c6452ce520ea16bbc6ddb1613
Sinjin <sinjin.hotlinebling@gmail.com>
2026-06-01 08:22:35 +0530

    added order table, need to change some fields

--- src/main/java/com/example/oms/orders/Order.java
+++ src/main/java/com/example/oms/orders/Order.java
@@ -1,9 +1,42 @@
 package com.example.oms.orders;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
+
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private OrderStatus orderStatus;
+
+    @OneToMany
+    @JoinColumn(name = "order_id")
     private List<OrderItem> orderItem;
+
+    @Min(value = 0)
+    private double totalAmount;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
